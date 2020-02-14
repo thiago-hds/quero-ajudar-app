f035f339eb32d3ff35855b0657d244d4ca778eb9
Thiago Santos <s.thiagoh@gmail.com>
2020-02-14 13:43:24 -0300

    Tela de splash iniciada

---
+++ app/src/main/java/com/br/queroajudar/Splash.kt
@@ -0,0 +1,22 @@
+package com.br.queroajudar
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
