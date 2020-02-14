ffb55ef2326f18024bd146c01840ad5b9c397c5e
Thiago Santos <s.thiagoh@gmail.com>
2020-02-14 14:50:09 -0300

    Tela de splash cont.

--- app/src/main/java/com/br/queroajudar/SplashActivity.kt
+++ app/src/main/java/com/br/queroajudar/SplashActivity.kt
@@ -6,17 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_splash.*
 
-class Splash : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
