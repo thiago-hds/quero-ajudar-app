64877c57ed759b36d3db1e6cee94c2e13b359833
Thiago Santos <s.thiagoh@gmail.com>
2020-03-12 09:35:51 -0300

    APP: layout de carregamento de recyclerview e objeto de constantes

--- app/src/main/java/com/br/queroajudar/util/Util.kt
+++ app/src/main/java/com/br/queroajudar/util/Util.kt
@@ -1,6 +1,13 @@
 package com.br.queroajudar.util
 
+object Constants {
+    const val VIEW_TYPE_ITEM    = 1
+    const val VIEW_TYPE_LOADING = 2
+    const val PAGE_SIZE         = 10
+}
+
 fun convertOrganizationNameToFormatted(name : String) : String{
     //TODO implementar funcao
     return name
 }
+
