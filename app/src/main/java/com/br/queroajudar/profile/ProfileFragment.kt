a19b56008bfebff1b7065f7d50ab1c67ed2ab0c2
thiagohs94 <s.thiagoh@gmail.com>
2020-10-11 12:26:34 -0300

    tela de perfil

--- app/src/main/java/com/br/queroajudar/profile/ProfileFragment.kt
+++ app/src/main/java/com/br/queroajudar/profile/ProfileFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 
 import com.br.queroajudar.R
+import com.br.queroajudar.databinding.FragmentProfileBinding
+import com.br.queroajudar.databinding.FragmentVacancyDetailsBinding
 import com.br.queroajudar.vacancies.HomeActivity
 import com.br.queroajudar.vacancydetails.VacancyDetailsViewModel
 import javax.inject.Inject
@@ -22,6 +25,8 @@ class ProfileFragment : Fragment() {
 
     private lateinit var viewModel : ProfileViewModel
 
+    lateinit var binding : FragmentProfileBinding
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (activity as HomeActivity).homeComponent.inject(this)
@@ -32,6 +37,16 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        viewModel = ViewModelProvider(this, viewModelFactory)[ProfileViewModel::class.java]
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_vacancy_details, container, false
+        )
+
+        binding.lifecycleOwner = this
+        //binding.viewModel = viewModel
+
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
